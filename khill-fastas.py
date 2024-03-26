5397483671fa4cf6fef520d2c83e8a382ac338f3
Dean Bobo <deanbobo@gmail.com>
2024-03-26 00:00:47 -0400

    Update khill-fastas.py
    many improvements and bug fixes.

--- khill-fastas.py
+++ khill-fastas.py
@@ -2,41 +2,104 @@
 
 from os import walk
 import sys
-import getopt
+import argparse
 import re
-import string
 import mmh3
-from urllib.request import urlopen
 import os
-
-#from multiprocessing import Process, Manager, Lock
 from math import log, exp
 
-def usage():
-    print('''
-    USAGE: 
-    -d <input_directory> : directory of genomes (optional, but either -d or -l must be provided)
-    -o <output_directory> : directory with output of run
-    -k <kmer_size> : kmer size
-    -m <motion> : defined kmer overlap; m=1 will move kmers along on nuc at time and so on
-    -n <number> : sampling rate
-    -p <procs> : forks
-    -c <cannon> : set to 1 if you want cannoical kmers (rev comp taken into account)
-    -s <kmer_lim> : limit of kmers to analyze
-    -l <filelist> : list of files (optional, but either -d or -l must be provided)
-    ''')
-    sys.exit()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Genome k-mer analysis tool.')
+    parser.add_argument('-d', '--directory', help='Directory of genomes (optional, but either --directory or --file_table must be provided)', default=None)
+    parser.add_argument('-o', '--output', help='Directory for output of run. Default is current working directory. It is recommended to specify an empty directory', default=os.getcwd())
+    parser.add_argument('-k', '--kmer_size', type=int, default=19, help='Kmer size')
+    parser.add_argument('-m', '--motion', type=int, default=1, help='Defined kmer overlap; 1 will move kmers along one nucleotide at a time')
+    parser.add_argument('-hs', '--hash_sketch', type=int, default=1, help='There are 2^64 hashes available for this hashing algorithm. 1 will look at every K-mer. 100 will examine 2^64/100 or 1 percent of K-mers. 100 is usually an acceptable value and saves computational time.')
+    parser.add_argument('-c', '--cannon', action='store_true', help='Enable canonical kmers (reverse complement taken into account).')
+    parser.add_argument('-mk', '--max_kmers', type=float, default=1e99, help='Limit the number of kmers to analyze')
+    parser.add_argument('-ft', '--file_table', help='Table of files. Requires 2 columns. First column is the the group_id, second column is the path to the fasta file. If --file_table is not provided, --directory must be used.')
+    parser.add_argument('-s', '--stats', help='File path for stats output', default='/Users/dean/Dropbox/amnh/kmers/khill/test_out/stats.out')
+    parser.add_argument('-f', '--force', action='store_true', help='Force overwrite of the stats output file if it already exists.')
+
+
+    return parser.parse_args()
 
 def revdnacomp(st):
     nn = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     return "".join(nn[n] for n in reversed(st))
 
 
-def log2(n):
-    return log(n) / log(2)
+def list_genomes(directory=None, file_table=None):
+    group_genomes = {}
 
-def log10(n):
-    return log(n) / log(10)
+    # Process file_table if provided
+    if file_table:
+        with open(file_table, 'r') as file:
+            for line in file:
+                parts = line.strip().split('\t')
+                if len(parts) == 2:
+                    group_id, genome_path = parts
+                    if group_id not in group_genomes:
+                        group_genomes[group_id] = []
+                    group_genomes[group_id].append(genome_path)
+
+    # Process directory if provided
+    if directory:
+        # Use the directory name or path as the default group name
+        # This can be customized based on your preference
+        default_group_id = path.basename(directory) if path.basename(directory) else "default_group"
+        group_genomes[default_group_id] = []
+        for dir_path, _, file_names in walk(directory):
+            for f in file_names:
+                if re.match(r".*\.(fasta|fa|fna)$", f):
+                    genome_path = path.join(dir_path, f)
+                    group_genomes[default_group_id].append(genome_path)
+
+    return group_genomes
+
+
+def check_input_files(groups):
+    missing_files = []
+    for group_id, genomes in groups.items():
+        for genome_path in genomes:
+            if not os.path.exists(genome_path):
+                missing_files.append(genome_path)
+
+    if missing_files:
+        print("Error: The following genome files do not exist:")
+        for file in missing_files:
+            print(file)
+        sys.exit(1)
+
+
+def process_genomes(genomes, args):
+    non_acgt_re = re.compile("[BDEFHIJKLMNOPQRSUVWXYZ0-9]", re.IGNORECASE)
+
+    for g in genomes:
+        filebase = os.path.basename(g)
+        hash_file_path = f"{args.output}/{filebase}.hashes"
+        
+        with open(hash_file_path, 'w') as HASHF, open(g, 'r') as FASTA:
+            seq = ""
+            for line in FASTA:
+                if not line.startswith('>'):
+                    seq += line.strip().upper()
+            
+            kc = 0
+            i = 0
+            while i + args.kmer_size <= len(seq):
+                ffrag = seq[i:i+args.kmer_size]
+                if not non_acgt_re.search(ffrag):
+                    cfrag = ffrag if not args.cannon else min(ffrag, revdnacomp(ffrag))
+                    the_hash = mmh3.hash64(cfrag, 460)[0]
+                    if the_hash < args.thresh:
+                        HASHF.write(f'{the_hash}\n')
+                    kc += 1
+                    if kc >= args.max_kmers:
+                        break
+                i += args.motion
+
 
 def kl(kl_g, kmers, kl_kpg, kl_sg, kl_allk):
     kl_val = 0
@@ -67,179 +130,114 @@ def kl(kl_g, kmers, kl_kpg, kl_sg, kl_allk):
 
     return kl_val
 
+def collate_hashes_write_betaent(genomes, args):
+    # collate kmers
+    kmers = {}
+    allkmers = 0
+    kmersperg = {}        
+    for g in genomes:
+        filebase = os.path.basename(g)
+        print(f"Collating hashes for {g}")
+        try:
+            with open(f"{args.output}/{filebase}.hashes", 'r') as HASHES:
+                for h in HASHES:
+                    h = h.strip()
+                    h=int(h)
+                    if h not in kmers:
+                        kmers[h] = {}
+                    if g not in kmers[h]:
+                        kmers[h][g] = 0
+                        kmers[h][g] += 1
+                    if g not in kmersperg:
+                        kmersperg[g] = 0
+                    kmersperg[g] += 1
+                    allkmers += 1
+        except FileNotFoundError:
+            print(f"Warning: Hash file for {filebase} not found.")
+
+    # calculate kl and write betaent
+    for sg in genomes:
+        filebase = os.path.basename(sg)
+        betaent_path = f"{args.output}/{filebase}.betaent"
+    
+        kl_val = kl(sg, kmers, kmersperg, genomes, allkmers) 
+    
+        with open(betaent_path, 'w') as f:
+            f.write(f"{filebase}\t{kl_val}\n")
 
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "d:o:k:p:m:n:c:s:l:")
-except getopt.GetoptError as err:
-    print(err)
-    usage()
-
-filelist = '/Users/dean/Dropbox/amnh/kmers/khill/filelist.txt'
-#indir = '/Users/dean/Dropbox/amnh/kmers/20220630/'
-outdir = '/Users/dean/Dropbox/amnh/kmers/khill/test_out/'
-kmer = 19
-motion = 1
-number = 1
-procs = 1
-cannon = 1
-kmerlim = 1e99
-
-for o, a in opts:
-    if o == "-d":
-        indir = a
-    elif o == "-o":
-        outdir = a
-    elif o == "-k":
-        kmer = int(a)
-    elif o == "-m":
-        motion = int(a)
-    elif o == "-n":
-        number = a
-    elif o == "-p":
-        procs = a
-    elif o == "-c":
-        cannon = a
-    elif o == "-s":
-        kmerlim = int(a)
-    elif o == "-l":
-        filelist = a
+def calculate_khill(group_id, genomes, args):
+    betaent = 0
+    for sg in genomes:
+        print(f"Calculating KL for {sg}")
+        filebase = os.path.basename(sg)
+        try:
+            with open(f"{args.output}/{filebase}.betaent", 'r') as HILL:
+                for line in HILL:
+                    parts = line.strip().split("\t")
+                    if len(parts) == 2:
+                        betaent += float(parts[1])
+        except FileNotFoundError:
+            print(f"Warning: Beta diversity file for {filebase} not found.")
+    
+    betadiv = exp(betaent)
+    
+    # Determine the stats file path outside of this function, if not already
+    stats_file_path = args.stats if args.stats else f"{args.output}/stats.txt"
+    
+    # Open in 'a' mode to append if the file exists after handling --force in main
+    with open(stats_file_path, 'a') as f:
+        # Check if the file is empty to decide whether to write the header
+        if os.stat(stats_file_path).st_size == 0:
+            f.write("#group_id\tbetaent\tbetadiv\n")
+        
+        # Write the group data
+        f.write(f"{group_id}\t{betaent}\t{betadiv}\n")
 
 
+def process_groups(groups, args):
+    for group_id, genomes in groups.items():
+        print(f"Processing group {group_id} with {len(genomes)} genomes.")
+        # Here you would adjust the functions called within this loop
+        # to handle processing of genomes within the same group together.
+        process_genomes(genomes, args)  # Adjust this function as necessary.
+        # You might need to modify process_genomes and related functions
+        # to handle genomes as a group, depending on how they're currently implemented.
+        collate_hashes_write_betaent(genomes, args) # collate hashes
+        calculate_khill(group_id, genomes, args)
 
-try: indir
-except: indir = None
+def main():
 
-try: filelist
-except: filelist = None
+    # get options
+    args = parse_arguments()
 
-#if (indir and filelist) is None or (indir or filelist) is not None:
-#    raise ValueError('Can only specify -d <in_dir> OR -l <file_list>. not both.')
+    # Custom check for --directory or --file_table
+    if not args.directory and not args.file_table:
+        print("Error: Either --directory or --file_table must be provided.")
+        sys.exit(1)  # Exit the program with an error code
+    
+    # check that the stats are going to print correctly
+    stats_file_path = args.stats if args.stats else f"{args.output}/stats.txt"
+    if os.path.isfile(stats_file_path) and not args.force:
+        print(f"Error: Stats file '{stats_file_path}' already exists. Use --force to overwrite.")
+        sys.exit(1)
+    elif os.path.isfile(stats_file_path) and args.force:
+        os.remove(stats_file_path)
+    
+
+    # Calculate thresh based on args.sampling_rate
+    maxhash = 2**64
+    args.thresh = maxhash / args.hash_sketch
+
+    # get the sample groups
+    groups = list_genomes(args.directory, args.file_table)
+
+    # check that all input files exist 
+    check_input_files(groups)
+
+    # process each genome
+    process_groups(groups, args) # generate hashes
 
 
-
-
-
-outdir=os.path.normpath(outdir)
-if indir:
-   indir=os.path.normpath(indir)
-
-genomes = []
-
-
-
-# list to store files name
-if indir:
-    res = []
-    for (dir_path, dir_names, file_names) in walk(indir):
-        res.extend(file_names)
-    r = re.compile(".*\.fasta|.*\.fa|.*\.fna" )
-    genomes = list(filter(r.match, res))
-
-if filelist is not None:
-    with open(filelist, 'r') as file:
-        lines = file.readlines()
-    genomes = [line.strip() for line in lines]
-
-print(genomes)
-
-maxhash = 2**64
-thresh = maxhash/number
-
-for g in genomes:
-    filebase = os.path.basename(g) 
-    HASHF=open(f"{outdir}/{filebase}.hashes", 'w')
-    print("counting kmers for " + g)
-    kc = 0
-    signal = 0
-    if indir:
-        FASTA = open(f"{indir}/{g}", 'r')
-    elif filelist:
-        FASTA = open(f"{g}", 'r')
-
-    for line in FASTA.readlines():
-        line = line.strip()
-        if line.startswith('>'):
-            next
-        else:
-            seq = line.upper()
-            cnter = 1
-            while(len(seq) >= kmer):
-                ffrag = seq[0:kmer]
-                
-                #disallow any string with unknown bases
-                if re.search("[BDEFHIJKLMNOPQRSUVWXYZ0-9]", ffrag, re.IGNORECASE):
-                    #print('Warning: Found non-ACGT character in fasta sequence.')
-                    seq = seq[motion:] #NOT SURE ABOT THIS
-                    cnter += motion
-                else:
-                    if cannon == 1:
-                        rcfrag = revdnacomp(ffrag)
-                        if ffrag < rcfrag:
-                            cfrag = ffrag
-                        else:
-                            cfrag = rcfrag
-                    else:
-                        cfrag = ffrag
-                    the_hash =  mmh3.hash64(cfrag, 460)[0]
-
-                    if the_hash < thresh:
-                        #print("we're here")
-                        HASHF.write('%d\n' % the_hash)
-                    kc=kc+1
-                    seq = seq[motion:]
-                    cnter += motion
-                if kc >= kmerlim:
-                    signal=signal+1
-                if signal > 0:
-                    break
-        if signal > 0:
-            break
-    FASTA.close()
-    HASHF.close()
-
-# collate hashses
-kmers = {}
-allkmers = 0
-kmersperg = {}
-for g in genomes:
-    filebase = os.path.basename(g)
-    print(f"Collating hashes for {g}")
-    HASHES = open(f"{outdir}/{filebase}.hashes", 'r')
-
-    for h in HASHES.readlines():
-        h = h.strip()
-        h=int(h)
-        if h not in kmers:
-            kmers[h] = {}
-        if g not in kmers[h]:
-            kmers[h][g] = 0
-        kmers[h][g] += 1
-        if g not in kmersperg:
-            kmersperg[g] = 0
-        kmersperg[g] += 1
-        allkmers += 1
-
-# KLdiv
-for sg in genomes:
-    filebase = os.path.basename(sg)
-    print(f"Calculating KL for {sg}")
-    kl_val = kl(sg, kmers, kmersperg, genomes, allkmers)
-    weight = (kmersperg[sg] / allkmers)
-    be = kl_val * weight
-    BETA = open(f"{outdir}/{filebase}.betaent", 'w')
-    BETA.write(f"{sg}\t{be}\t{kl_val}\t{weight}\n")
-
-# effective number of genomes (hill number for beta diversity)
-betaent = 0
-for sg in genomes:
-    filebase = os.path.basename(sg)
-    HILL=open(f"{outdir}/{filebase}.betaent", 'r')
-    for line in HILL.readlines():
-        line = line.strip()
-        line = line.split("\t")
-        betaent += float(line[1])
-betadiv = exp(betaent)
-with open(f"{outdir}/stats.txt", 'w') as f:
-    f.write(f"betaEnt={betaent}\n")
-    f.write(f"betaDiv={betadiv}\n")
+if __name__ == "__main__":
+    main()
